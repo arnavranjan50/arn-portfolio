f57f26a6bf32252e953eb1732ba36d63d481eb1c
unknown <trlalic1030u@gmail.com>
2026-05-22 09:49:35 +0530

    Remove custom cursor effect, use default Windows cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,12 @@ import StatsSkillsSection from "@/components/StatsSkillsSection";
 import ContactSection from "@/components/ContactSection";
 import DecorativeVisual from "@/components/DecorativeVisual";
 import Footer from "@/components/Footer";
-import CustomCursor from "@/components/CustomCursor";
 import Preloader from "@/components/Preloader";
 
 export default function Home() {
   return (
     <main>
       <Preloader />
-      <CustomCursor />
       <Navbar />
       <Hero />
       <AboutSection />
